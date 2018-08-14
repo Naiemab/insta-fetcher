b5115fa8bd6f0ff65c24280dfd51f65abf6e8bab
Naiem <naiem@localhost.localdomain>
2018-08-14 15:35:26 +0430

    added soft delete

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @property mixed name
  */
 class Campaign extends Model
 {
+    use SoftDeletes;
     protected $guarded = [];
     protected $fillable = [];
+    protected $dates = ['deleted_at'];
 }
